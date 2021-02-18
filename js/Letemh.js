be339f4ac0d1e5ef872b7a7067e47c0c2d2d3472
jintaovo <1712421071@qq.com>
2021-02-18 21:32:59 +0800

    update

--- js/Letemh.js
+++ js/Letemh.js
@@ -114,12 +114,25 @@ if (!(bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsC
 
 
 // 设置页脚博主
-$(document).ready(function(e) {
-    // 自己的请自行更改
-    var since = 2020;
-    var name = '廿廿难辞';
-    var now = new Date();
-    var nowYear = now.getFullYear();
-    since = since == nowYear?since:since+" - "+nowYear;
-    $('.copyright').html('©'+since+' <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> '+name);
-});
+// $(document).ready(function(e) {
+//     // 自己的请自行更改
+//     var since = 2020;
+//     var name = 'Lete乐特';
+//     var now = new Date();
+//     var nowYear = now.getFullYear();
+//     since = since == nowYear?since:since+" - "+nowYear;
+//     $('.copyright').html('©'+since+' <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> '+name);
+// });
+/*添加图片top*/
+var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif' title='回到顶部' >";
+/*添加到返回顶部按钮下*/
+document.getElementById("go-up").innerHTML += top_up;
+
+/* 手机客户端导航栏默认隐藏 */
+var mobile_sidebar_menus = document.getElementById("mobile-sidebar-menus");
+var menus_item_child = mobile_sidebar_menus.getElementsByClassName("menus_item_child");
+var menus_expand = mobile_sidebar_menus.getElementsByClassName("expand");
+for (var i = 0; i < menus_item_child.length; i++) {
+    menus_item_child[i].style.display = "none";
+    menus_expand[i].className += " closed";
+}
