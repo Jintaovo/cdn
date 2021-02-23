2ddaf5cf1099d4101432e94940398058574efa22
jintaovo <1712421071@qq.com>
2021-02-23 12:31:46 +0800

    update

--- js/Lete1.js
+++ js/Lete1.js
@@ -8,27 +8,6 @@
 
 /*添加图片top*/
 // 判断移动端设备
-browserRedirect();
-
-function browserRedirect() {
-    var sUserAgent = navigator.userAgent.toLowerCase();
-    var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
-    var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
-    var bIsMidp = sUserAgent.match(/midp/i) == "midp";
-    var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
-    var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
-    var bIsAndroid = sUserAgent.match(/android/i) == "android";
-    var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
-    var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
-    if (!(bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM)) {
-        var top_up = "<img class='upj' src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif'>";
-        /*添加到返回顶部按钮下*/
-        document.getElementById("go-up").innerHTML += top_up;
-    }
-    
-}
-
-
 
 // 可爱的Title
 var OriginTitle = document.title;
@@ -128,11 +107,3 @@ if (!(bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsC
 // /*添加到返回顶部按钮下*/
 // document.getElementById("go-up").innerHTML += top_up;
 
-/* 手机客户端导航栏默认隐藏 */
-var mobile_sidebar_menus = document.getElementById("mobile-sidebar-menus");
-var menus_item_child = mobile_sidebar_menus.getElementsByClassName("menus_item_child");
-var menus_expand = mobile_sidebar_menus.getElementsByClassName("expand");
-for (var i = 0; i < menus_item_child.length; i++) {
-    menus_item_child[i].style.display = "none";
-    menus_expand[i].className += " closed";
-}
